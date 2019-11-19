afcfd28d62df5a0921881f58ff794a37151a8de1
Adam Sulewski <adam.j.sulewski@gmail.com>
2019-11-19 07:39:19 -0500

    add play and pause

--- lib/spotify/player.ex
+++ lib/spotify/player.ex
@@ -1,2 +1,26 @@
 defmodule Spotify.Player do
+  alias Spotify.Client
+  use Spotify.Responder
+
+  def play(conn) do
+    conn |> Client.put(play_url()) |> handle_response()
+  end
+
+  def play(conn, body) do
+    conn |> Client.put(play_url(), body) |> handle_response()
+  end
+
+  def play_url do
+    "https://api.spotify.com/v1/me/player/play"
+  end
+
+  def pause(conn) do
+    conn |> Client.put(pause_url()) |> handle_response()
+  end
+
+  def pause_url do
+    "https://api.spotify.com/v1/me/player/pause"
+  end
+
+  def build_response(body), do: body
 end
